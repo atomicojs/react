2bc8693ca0952593c2055c5e5a90713e05ee2ca1
UpperCod <mtrujillo@outlook.cl>
2021-05-31 04:16:09 -0400

    update pkg

--- wrapper.d.ts
+++ wrapper.d.ts
@@ -1,4 +1,4 @@
-export function wrapper<T>(tagName: string, base?: T): T extends Atomico ? Component<T["Props"]> : Component<any>;
+export function wrapper<T>(tagName: string, base?: T): T extends Atomico ? Component<Partial<T["Props"]>> : Component<any>;
 export type Atomico = {
     Props: {
         [x: string]: any;
