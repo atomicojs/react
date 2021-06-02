0dacf0b8d4d768ac00511129e4badf9f4294de06
UpperCod <mtrujillo@outlook.cl>
2021-06-02 10:39:14 -0400

    move logic from Partial to Component type

--- wrapper.d.ts
+++
@@ -1,8 +0,0 @@
-export function wrapper<T>(tagName: string, base?: T, { extends: is }?: ElementDefinitionOptions): T extends Atomico ? Component<T["Props"], T> : Component<{}, T>;
-export type Fill = {
-    [x: string]: any;
-};
-export type Atomico = {
-    Props: Fill;
-};
-export type Component<P, C> = (props: Partial<P> & import("react").DOMAttributes<C> & Fill) => any;
