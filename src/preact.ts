f6f50f3da71d24fac7890037ca44458d270b186d
UpperCod <uppercod.git@gmail.com>
2022-07-03 14:13:55 -0400

    remove preact/compact

--- src/preact.ts
+++ src/preact.ts
@@ -1,8 +1,12 @@
 import { createElement } from "preact";
-import { forwardRef } from "preact/compat";
 import { useLayoutEffect, useRef } from "preact/hooks";
 import { createAuto } from "./core/create-auto";
 import { createWrapper } from "./core/create-wrapper";
+
+const forwardRef =
+  (component: (props: any, ref: any) => any) =>
+  ({ ref, ...props }: any) =>
+    component(props, ref);
 
 export const wrapper = createWrapper({
   createElement,
